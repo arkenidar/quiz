365c2ea6dd179ac9d5ae76a7a1d1730951a4350a
Dario Cangialosi Arkenidar (DCA) <dario.cangialosi@gmail.com>
2023-06-22 22:25:45 +0200

    refinements

--- new/answers.php
+++ new/answers.php
@@ -1,17 +1,30 @@
-<!doctype html>
+<!DOCTYPE html>
+<html lang="en"> <head>
+<title>quiz answers</title>
+
 <meta name="viewport" content="width=device-width, initial-scale=1">
-solutions...<br>
+<meta charset="UTF-8">
+
+</head> <body>
+
+<h2>Solutions...</h2>
 <?php $questions=json_decode(file_get_contents('set'.$_REQUEST['set'].'.json')); ?>
+
+<ol>
 <?php foreach($questions as $question){ ?>
-<div><?=$question[0]?><br><?=$question[1][$question[2]]?></div>
+    <li> <div><?=$question[0]?></div> <div><?=$question[1][$question[2]-1]?></div> </li>
 <?php } ?>
+</ol>
+
 <?php
 $correct_count=0;
 $count=count($questions);
 foreach($_REQUEST['answers'] as $answer_index=>$answer){
-$correct=$questions[$answer_index][2]==$answer;
+$correct=($questions[$answer_index-1][2])==$answer;
 if($correct) $correct_count++;
 }
 ?>
+<h2>Punteggio</h2>
 <div><?=$correct_count?>/<?=$count?> correct</div>
 
+</body> </html>
--- new/questions.php
+++ new/questions.php
@@ -1,16 +1,35 @@
-<!doctype html>
+<!DOCTYPE html>
+<html lang="en"> <head>
+<title>quiz questions</title>
+
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<meta charset="UTF-8">
+
+</head> <body>
+
+<h2>Questions</h2>
+
 <form action="answers.php" method="post">
 <input type="hidden" name="set" value="<?=htmlspecialchars($_REQUEST['set'])?>">
-<?php foreach(json_decode(file_get_contents('set'.$_REQUEST['set'].'.json')) as $quest_idx=>$question){ ?>
-<div><?=$question[0]?><br>
-<?php foreach($question[1] as $poss_idx=>$possibility){ ?>
-<input type="radio" name="answers[<?=$quest_idx?>]" value="<?=$poss_idx?>"
-id="id_<?=$quest_idx?>_<?=$poss_idx?>"><label for="id_<?=$quest_idx?>_<?=$poss_idx?>">
-<?=$possibility?></label><br>
+
+<ol>
+<?php foreach(json_decode(file_get_contents('set'.$_REQUEST['set'].'.json'))
+    as $quest_idx=>$question){ ?>
+    
+    <li><?=$question[0]?><br>
+    <ol>
+    <?php foreach($question[1] as $poss_idx=>$possibility){ ?>
+    <li><label> <input type="radio" name="answers[<?=$quest_idx+1?>]" value="<?=$poss_idx+1?>" id="id_<?=$quest_idx+1?>_<?=$poss_idx+1?>">
+        <?=$possibility?> 
+        </label></li>
+
+    <?php } ?>
+    </ol>
+
+    </li>
 <?php } ?>
-</div>
-<?php } ?>
+</ol>
 <input type="submit">
 </form>
 
+</body> </html>
